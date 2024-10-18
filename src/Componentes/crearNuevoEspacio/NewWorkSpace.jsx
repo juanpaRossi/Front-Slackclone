636fc98695f04209f830258e7162a6ffcc042b96
Juan Pablo Rossi <juanpafbf@gmail.com>
2024-10-17 22:29:46 -0300

    arreglo de importaciones

--- src/Componentes/crearNuevoEspacio/NewWorkSpace.jsx
+++ src/Componentes/crearNuevoEspacio/NewWorkSpace.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './formNewWs.css'
+import './formNewWS.css'
 import { crearWorkspace, obtenerWorkspaces } from '../data'
 import {  useNavigate } from 'react-router-dom'
 const NewWorkSpace = () => {
